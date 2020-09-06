0f3e06af40c149bc44df83ed93d7cd54be5a823c
kathryn-sanders <kathryn.sanders.ks@gmail.com>
2020-09-05 23:17:05 -0400

    added walet

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <StateViewer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import StateViewer from '@/components/StateViewer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    StateViewer
   }
 }
 </script>
