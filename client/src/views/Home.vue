7a484ed78bb933b2299fee8e9ba543770df18787
shinymeta <zach.swalberg@gmail.com>
2020-09-07 20:14:42 -0400

    moved state fetching logic to   StateViewer

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="home">
-    <span> Enter API Key: </span>
-    <input type="text" v-model="apikey" />
-    <button @click="getState"> Request Account State </button>
+    <iframe hidden=true name="dummyFormSubmitTargetFrame"></iframe>
+    <form @submit="submitApikey" target="dummyFormSubmitTargetFrame">
+      <label for="apikeyInput"> Enter API Key: </label>
+      <input type="text" v-model="apikey"
+        pattern="[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{20}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}"
+        id="apikeyInput"
+        />
+      <button type="submit"> Request Account State </button>
+    </form>
 
     <StateViewer 
-      :accountState="accountState"
-      :currencyLookup="currencyLookup"
-      :itemLookup="itemLookup"
+      :api="api"
+      :apikeyAuthenticated="apikeyAuthenticated"
       />
   </div>
 </template>
@@ -19,7 +24,6 @@ import StateViewer from '@/components/StateViewer.vue'
 const client = require('gw2api-client')
 
 // Get an instance of an API client
-let api = client()
 
 export default {
   name: 'Home',
@@ -28,7 +32,9 @@ export default {
   },
   data() {
     return {
+      api: client(),
       apikey: 'CFBA11E8-CDAD-144E-82DF-1F3E8E1D7D855C321BCE-88F2-46D5-A27F-0DA6FC88F39A',
+      apikeyAuthenticated: false,
       accountState: {
         wallet:[],
         bank:[],
@@ -37,103 +43,24 @@ export default {
         characters:[],
       },
       currencyLookup:{},
-      itemLookup:{}
+      itemLookup:{},
     }
   },
   methods:{
-    getState() {
-      api.authenticate(this.apikey)
-
-      api.account().wallet().get().then((wallet) => {
-        this.accountState.wallet = wallet
-        // console.log("wallt:")
-        // console.log(wallet)
-      })
-      api.account().bank().get().then((bank) => {
-        this.accountState.bank = bank
-
-            let itemIDSet = new Set()
-            bank.forEach((bankSlot) => {
-              if (bankSlot) { itemIDSet.add(bankSlot.id) }
-            })
-            this.getItemsForLookup(Array.from(itemIDSet))
-
-        // console.log("bank:")
-        // console.log(bank)
-      })
-      api.account().materials().get().then((materialStorage) => {
-        this.accountState.materialStorage = materialStorage
-
-            let itemIDSet = new Set()
-            materialStorage.forEach((material) => {
-              if (material) { itemIDSet.add(material.id) }
-            })
-            this.getItemsForLookup(Array.from(itemIDSet))
-
-        // console.log("matStore:")
-        // console.log(materialStorage)
-      })
-      api.account().inventory().get().then((sharedInventory) => {
-        this.accountState.sharedInventory = sharedInventory
-
-            let itemIDSet = new Set()
-            sharedInventory.forEach((sharedSlot) => {
-              if (sharedSlot) { itemIDSet.add(sharedSlot.id) }
-            })
-            this.getItemsForLookup(Array.from(itemIDSet))
-
-        // console.log("shared slotes:")
-        // console.log(sharedInventory)
-      })
-      api.account().characters().all().then((characters) => {
-        this.accountState.characters = characters
-
-            let itemIDSet = new Set()
-            this.forEachItemInCharactersInventory(characters, (item) => {
-              itemIDSet.add(item.id)
-            })
-            this.getItemsForLookup(Array.from(itemIDSet))
-
-        // console.log("chars:")
-        // console.log(characters)
-      })
-    },
-
-    getCurrenciesForLookup(){
-      api.currencies().all().then((currencies) => {
-        // console.log("currencies:")
-        // console.log(currencies)
-        currencies.forEach((currency) => {
-          this.currencyLookup[currency.id] = currency
+    submitApikey() {
+      this.api.authenticate(this.apikey)
+      this.api.account().get()
+        .then(() => {
+          this.apikeyAuthenticated = true
         })
-      })
-    },
-
-    getItemsForLookup(ids) {
-      console.log(ids)
-      api.items().many(ids).then((items) => {
-        // console.log("items:\n")
-        // console.log(items)
-        items.forEach((item) => {
-          this.itemLookup[item.id] = item
+        .catch((err) => {
+          alert('API Key did not Authenticate successfully.  Please try again.')
+          console.error(err)
+          this.apikeyAuthenticated = false
         })
-      })
     },
-
-    forEachItemInCharactersInventory(characters, forEachCallback) {
-      characters.forEach((character) => {
-        character.bags.forEach((bag) => { if(bag) {
-          bag.inventory.forEach((bagInventorySlot) => { if (bagInventorySlot) {
-            forEachCallback(bagInventorySlot)
-          }})
-        } })
-      })
-    }
   },
 
-  mounted() {
-    this.getCurrenciesForLookup()
-  }
 
 }
 </script>
